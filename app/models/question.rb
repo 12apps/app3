2dd6b6d3eada32961b91112fbd88dbb094b185f8
CodingHouse <sb@codinghouse.co>
2014-12-14 23:44:33 -0800

    Guard for email

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,13 @@
 class Question < ActiveRecord::Base
 
+  validates :email, presence: true
+
   def as_json(options)
     super(options.merge(methods: [:gravatar_url]))
   end
 
   def gravatar_url
-    Gravatar.new(email).image_url
+    Gravatar.new(email).image_url if email
   end
 
 end
